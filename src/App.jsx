4ab9e5461dc515c7666501180019bf671f47d780
John Prabu A <johnprabu0702@gmail.com>
2024-11-19 02:24:17 +0530

    Certificates Templates created for courses, workshops and events and sample routes added for testing purposes and all the required routes created with sample component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import CourseCertificateComponent from "./components/CertificateTemplates/CourseCertificateComponent";
+import EventCertificateComponent from "./components/CertificateTemplates/EventCertificateComponent";
+import WorkShopCertificateComponent from "./components/CertificateTemplates/WorkShopCertificateComponent";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Digital Certificate Wallet</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/certificates/:st_id" element={<h1>Student Certificates Page</h1>} />
+        <Route path="/certificates/:st_id/:cert_id" element={<h1>Individual Certificate Page</h1>} />
+        <Route path="/dashboard/:st_id" element={<h1>Student Details Page</h1>} />
+        <Route path="*" element={<NotFoundPage />} />
+
+        // only for testing 
+        <Route path="/course" element={<CourseCertificateComponent 
+            studentName="John" 
+            courseName="GenAI Study Jam"
+            date="12-11-2024"
+            skills={["The Basics of Google Cloud Compute","Analyze Images with the Cloud Vision API","Prompt Design in Vertex AI"]}
+            />}
+         />
+        <Route path="/event" element={<EventCertificateComponent 
+            studentName="John"
+            eventName="Ideathon"
+            date="13-11-2024"
+            />} 
+        />
+        <Route path="/workshop" element={<WorkShopCertificateComponent 
+            studentName="John" 
+            workshopName="Web Development Workshop" 
+            date="14-11-2024" 
+          />
+          } 
+        />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
