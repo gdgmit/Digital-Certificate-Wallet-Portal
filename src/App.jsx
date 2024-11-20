a5211b53070939c3722afdfa61a0cf75f3eb16c8
Thasneem Fathima <thasneemf78@gmail.com>
2024-11-20 22:50:19 +0530

    task 6 merged

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import LoginPage from "./pages/LoginPage";
-import NotFoundPage from "./pages/NotFoundPage";
 import CourseCertificateComponent from "./components/CertificateTemplates/CourseCertificateComponent";
 import EventCertificateComponent from "./components/CertificateTemplates/EventCertificateComponent";
 import WorkShopCertificateComponent from "./components/CertificateTemplates/WorkShopCertificateComponent";
@@ -11,14 +8,6 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/certificates/:st_id" element={<h1>Student Certificates Page</h1>} />
-        <Route path="/certificates/:st_id/:cert_id" element={<h1>Individual Certificate Page</h1>} />
-        <Route path="/dashboard/:st_id" element={<h1>Student Details Page</h1>} />
-        <Route path="*" element={<NotFoundPage />} />
-
-        // only for testing 
         <Route path="/course" element={<CourseCertificateComponent 
             certificateData={studentsData[0].certificates[1]}
             studentName={studentsData[0].st_name}
--- src/pages/NotFoundPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function NotFoundPage() {
-  return (
-    <div>404 Page Not Found</div>
-  )
-}
-
-export default NotFoundPage
