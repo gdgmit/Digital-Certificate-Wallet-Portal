5384a36efa61c1ea5e599317c61033d5c5aec5c2
Hariharan <hariharan17kpm@gmail.com>
2025-01-02 18:32:32 +0530

    Task 3 Completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,41 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import CourseCertificateComponent from "./components/CertificateTemplates/CourseCertificateComponent";
 import EventCertificateComponent from "./components/CertificateTemplates/EventCertificateComponent";
 import WorkShopCertificateComponent from "./components/CertificateTemplates/WorkShopCertificateComponent";
+import CertificateDetails from "./components/PreviewComponents/CertificateDetails";
 import studentsData from "./data/certificate_data.json";
+
 function App() {
-  
   return (
     <Router>
       <Routes>
-        <Route path="/course" element={<CourseCertificateComponent 
-            certificateData={studentsData[0].certificates[1]}
-            studentName={studentsData[0].st_name}
-            />}
-         />
-        <Route path="/event" element={<EventCertificateComponent 
-            studentName={studentsData[1].st_name}
-            certificateData={studentsData[1].certificates[0]}
-            />} 
+        <Route
+          path="/course"
+          element={
+            <CourseCertificateComponent
+              certificateData={studentsData[0].certificates[1]}
+              studentName={studentsData[0].st_name}
+            />
+          }
         />
-        <Route path="/workshop" element={<WorkShopCertificateComponent 
-            studentName={studentsData[0].st_name} 
-            certificateData={studentsData[0].certificates[0]}
-          />
-          } 
+        <Route
+          path="/event"
+          element={
+            <EventCertificateComponent
+              studentName={studentsData[1].st_name}
+              certificateData={studentsData[1].certificates[0]}
+            />
+          }
         />
+        <Route
+          path="/workshop"
+          element={
+            <WorkShopCertificateComponent
+              studentName={studentsData[0].st_name}
+              certificateData={studentsData[0].certificates[0]}
+            />
+          }
+        />
+        <Route path="/certificates/:st_id/:cert_id" element={<CertificateDetails />} />
       </Routes>
     </Router>
   );
